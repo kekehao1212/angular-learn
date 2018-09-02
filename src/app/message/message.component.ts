4f4f4a1640e7116caa8e69bbf237eab8fd249fe4
kekehao <kekehao1212@outlook.com>
2018-09-02 22:38:30 +0800

    add @Input value Message Interface

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 import { MessageService } from '../message.service';
 import { EventEmitter } from '@angular/core';
 
+interface Message {
+  h: string;
+}
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-  @Input() message: object;
+  @Input() message: Message;
   @Output() hello: EventEmitter<any> = new EventEmitter();
   click(event): void {
     this.hello.emit(null);
