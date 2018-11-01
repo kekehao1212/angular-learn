aeae9a021c9c44771ad84e975227aaf21ef184f8
kekehao <kekehao1212@outlook.com>
2018-11-01 20:48:14 +0800

    joke step4 fetch new data on demand

--- src/module/jokes/joke-list/joke-list.component.ts
+++ src/module/jokes/joke-list/joke-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Subject, merge } from 'rxjs';
 import { Joke } from '../types';
 import { JokeService } from '../services/joke.service';
-import { take, mergeMap, skip, mapTo } from 'rxjs/operators';
+import { take, mergeMap, skip, mapTo, switchMap } from 'rxjs/operators';
 import { Memoize } from 'lodash-decorators';
 @Component({
   selector: 'app-joke-list',
@@ -13,21 +13,28 @@ export class JokeListComponent implements OnInit {
   jokes$: Observable<Array<Joke>>;
   update$ = new Subject<void>();
   showNotification$: Observable<boolean>;
+  forceReload$ = new Subject<void>();
   constructor(
     private jokeService: JokeService
   ) { }
 
   ngOnInit() {
     const intialJokes$ = this.getDataOnce();
-    const updates$ = this.update$.pipe(
+    const updates$ = merge(this.forceReload$, this.update$).pipe(
       mergeMap(() => this.getDataOnce())
     );
     this.jokes$ = merge(intialJokes$, updates$);
 
-    const intinalNotifications$ = this.jokeService.jokes.pipe(skip(1));
-    const show$ = intinalNotifications$.pipe(mapTo(true));
+    const reload$ = this.forceReload$.pipe(switchMap(() => this.getNotifications()));
+    const intinalNotifications$ = this.getNotifications();
+    const show$ = merge(reload$, intinalNotifications$).pipe(mapTo(true));
     const hide$ = this.update$.pipe(mapTo(false));
     this.showNotification$ = merge(show$, hide$);
+  }
+
+  forceReload() {
+    this.jokeService.forceReload();
+    this.forceReload$.next();
   }
 
   @Memoize()
@@ -37,5 +44,8 @@ export class JokeListComponent implements OnInit {
 
   getDataOnce() {
     return this.jokeService.jokes.pipe(take(1));
+  }
+  getNotifications() {
+    return this.jokeService.jokes.pipe(skip(1));
   }
 }
