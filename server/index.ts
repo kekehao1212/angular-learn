42daf3ae0ca81f19e8fabfe0b695f8e316ecaa7a
kekehao <kekehao1212@outlook.com>
2018-11-08 17:08:41 +0800

    server finish

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import { verify, sign } from 'jsonwebtoken';
 
 const server = jsonServer.create();
 const SECRET_KEY = '123456';
-const EXPRIES = '1h';
+const EXPIRES_IN = '1h';
 
 server.use(jsonServer.defaults());
 server.use(bodyParser.urlencoded({ extended: true }));
@@ -16,24 +16,82 @@ server.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4201;
 const DB = JSON.parse(readFileSync(join(__dirname, 'db.json'), 'UTF-8'));
+console.log(DB);
 
 const AUTH_ROUTER = express.Router();
 const API_ROUTER = express.Router();
 const JSON_SERVER_ROUTER = jsonServer.router(DB);
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    const status = 401;
+    res.status(status).json({ status, message: 'Bad authorization hearde'});
+    return;
+  }
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    verify(token, SECRET_KEY);
+    next();
+  } catch (err) {
+    const status = 401;
+    res.status(401).json({ status, message: 'Access token is not valid, try to login agagin'});
+  }
+});
+
 
 AUTH_ROUTER.post('/login', (req, res) => {
   const { username, password } = req.body;
   const expiresIn = req.query.expiresIn;
 
   if (!isAuthenticated({ username, password })) {
+    console.log(username, password);
     const status = 401;
     res.status(status).json({ status, message: 'Incorrect emain or password'});
     return;
   }
-  const accessToken = sign({ username, password }, SECRET_KEY, { expriesIn: expiresIn || EXPIRES_IN });
+  const accessToken = sign({ username, password }, SECRET_KEY, { expiresIn: expiresIn || EXPIRES_IN });
   res.status(200).json({ accessToken });
 });
 
+AUTH_ROUTER.get('/forgotPassword', (req, res) => {
+  const user = DB.users.find(userEntity => userEntity.username === req.query.username);
+
+  if (!user) {
+    const status = 404;
+    res.status(status).json({ status, message: 'Username does not exist'});
+    return;
+  }
+
+  res.status(200).json({ password: user.password });
+});
+
+API_ROUTER.post('/reset', (req, res) => {
+  console.log('Resetting database...');
+  JSON_SERVER_ROUTER.db.set('todos[]', []).write();
+
+  return res.status(200).json({ message: 'DB successfully reset'});
+});
+
+API_ROUTER.put('/toggleAll', (req, res) => {
+  const completed = req.query.completed;
+  const todos = JSON_SERVER_ROUTER.db.get('todos').value();
+
+  todos.forEach(todo => (todo.completed = completed.toLowerCase() === 'true'));
+  JSON_SERVER_ROUTER.db.set('todos', todos).write();
+
+  res.status(200).json(todos);
+});
+
+API_ROUTER.put('/clearCompleted', (req, res) => {
+  const todos = JSON_SERVER_ROUTER.db.get('todos').value();
+
+  const updatedTodos = todos.filter(todo => !todo.completed);
+  JSON_SERVER_ROUTER.db.set('todos', updatedTodos).write();
+
+  res.status(200).json(updatedTodos);
+});
+
+API_ROUTER.use('/', JSON_SERVER_ROUTER);
 server.use('/auth', AUTH_ROUTER);
 server.use('/api', API_ROUTER);
 
@@ -42,5 +100,5 @@ server.listen(PORT, () => {
 });
 
 function isAuthenticated({ username, password }) {
-  return DB.users.findIndex(user => user.username === username && user.password === password);
+  return DB.users.findIndex(user => user.username === username && user.password === password) !== -1;
 }
